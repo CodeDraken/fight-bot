1b1b2df8f5980ced4e6ef7bec857ac1edbf44ee0
CodeDraken <codedraken@gmail.com>
2019-06-11 18:24:07 -0500

    Prevent command injection in tldr command

--- src/commands/tldr.js
+++ src/commands/tldr.js
@@ -7,16 +7,20 @@ module.exports = {
   async execute (message, args) {
     args = Array.isArray(args) ? args.join(' ') : args
 
-    exec(`npm run tldr ${args}`, (err, stdout, stderr) => {
-      if (err) throw err
-      if (stderr) throw stderr
+    exec(`npm run tldr "${args}"`, (err, stdout, stderr) => {
+      try {
+        if (err) throw err
+        if (stderr) throw stderr
 
-      if (stdout) {
-        const msg = stdout.split('\n').slice(5).join('\n')
-        return message.channel.send(
-          `**${args}**\n\`\`\`${msg}\`\`\``
-        )
-      } else {
+        if (stdout) {
+          const msg = stdout.split('\n').slice(5).join('\n')
+          return message.channel.send(
+            `**${args}**\n\`\`\`${msg}\`\`\``
+          )
+        } else {
+          return message.channel.send(`I couldn't find that page.`)
+        }
+      } catch (e) {
         return message.channel.send(`I couldn't find that page.`)
       }
     })
